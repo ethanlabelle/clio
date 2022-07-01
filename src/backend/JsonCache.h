940a494f2eafbd260db37849e0c0309152cbf1f6
Ethan LaBelle <ethan.labelle01@gmail.com>
2022-07-01 20:44:39 +0000

    clang format

--- src/backend/JsonCache.h
+++ src/backend/JsonCache.h
@@ -21,14 +21,18 @@ class JsonCache
         uint32_t seq = 0;
         boost::json::object obj;
     };
-    mutable std::shared_mutex mtx_; 
-    gtl::lru_cache<ripple::uint256, CacheEntry> map_;        
+    mutable std::shared_mutex mtx_;
+    gtl::lru_cache<ripple::uint256, CacheEntry> map_;
+
 public:
     std::optional<boost::json::object>
     get(ripple::uint256 const& key, uint32_t seq);
 
     void
-    insert(ripple::uint256 const& key, boost::json::object const& value, uint32_t seq);
+    insert(
+        ripple::uint256 const& key,
+        boost::json::object const& value,
+        uint32_t seq);
 };
-} // namespace Backend
+}  // namespace Backend
 #endif
