c44a6908783c4502f6bf489695ea54a7d3a20eba
Ethan LaBelle <ethan.labelle01@gmail.com>
2022-07-01 20:44:06 +0000

    Json cache impl

--- src/backend/JsonCache.cpp
+++ src/backend/JsonCache.cpp
@@ -1,1 +1,22 @@
 #include <backend/JsonCache.h>
+namespace Backend {
+
+std::optional<boost::json::object>
+JsonCache::get(ripple::uint256 const& key, uint32_t seq) {
+    std::shared_lock lk(mtx_);
+    auto cached = map_.get(key);
+    if (cached == nullptr || cached->seq != seq) {
+        return {};
+    } else {
+        return {cached->obj};
+    }
+}
+
+void
+JsonCache::insert(ripple::uint256 const& key, boost::json::object const& value, uint32_t seq) {
+    std::unique_lock lk(mtx_);
+    CacheEntry e  = {seq, value};
+    map_.insert(key, e);
+} 
+
+} // namespace Backend
